7bb4a75f501783cb3b0d7e3d2ec82731cba99872
Jonathan Nelson <jonathanator14@gmail.com>
2025-07-02 01:02:08 -0500

    complete

--- abbreviate-a-two-word-name/solution.go
+++ abbreviate-a-two-word-name/solution.go
@@ -1,6 +1,12 @@
 package kata
 
+import (
+	"strings"
+)
+
 func AbbrevName(name string) string{
-  //your code here
-  return ""
+  splitNames := strings.Split(name," ")
+  fUpper := strings.ToUpper(string(splitNames[0][0]))
+  lUpper := strings.ToUpper(string(splitNames[1][0]))
+  return fUpper + "." + lUpper
 }
